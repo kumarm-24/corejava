02d49830dfb87c202464f91315e96eaee1a8eeeb
kumarm-24 <kumar424.km@gmail.com>
2025-11-07 10:24:02 +0530

    Constructor basic problem

---
+++ src/com/java/oops/Constructor.java
@@ -0,0 +1,37 @@
+package com.java.oops;
+
+class Student {
+    int id;
+    String name;
+
+    Student() {
+        id = 0;
+        name = "Unknown";
+    }
+
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    void display() {
+        System.out.println(id + " " + name);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student(102, "Ravi");
+        s1.display();
+        s2.display();
+    }
+}
+
+
+
+
+
+
+
+
+
+
